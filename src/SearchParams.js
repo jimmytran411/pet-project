ae40fe9878e4c97fc4250e9dacead698b719be5f
An Tran <jimmytran411@gmail.com>
2020-12-29 14:18:00 +0200

    modified .eslintrc + add async function in SearchParams

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -2,13 +2,24 @@
 import React ,{useState, useEffect} from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from './useDropdown';
-
+import Results from './Results.js';
 
 const SearchParams = () => {
     const [location, setLocation] = useState("Seattle, WA");
     const [breeds, setBreeds] = useState([]);
     const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
     const [breed, BreedDropdown, setBreed] = useDropdown("Breed","",breeds);
+    const [pet, setPets] = useState([]);
+
+    async function requestPets() {
+        const {animals} = await pet.animals({
+            location,
+            breed,
+            type: animal
+        })
+
+        setPets(animals || []);
+    }
 
     useEffect( () => {
         setBreeds([]);
@@ -22,7 +33,10 @@ const SearchParams = () => {
 
     return(
         <div className="research-params">
-            <form>
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                requestPets();
+            }}>
                 <label htmlFor="location">Location
                 <input
                 id="location"
@@ -33,6 +47,7 @@ const SearchParams = () => {
             </form>
             <AnimalDropdown />
             <BreedDropdown />
+            <Results />
         </div>
     );
 }
