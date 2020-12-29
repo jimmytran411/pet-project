f34f3889877dba4a9a6cfb3349bfec79ed14e404
An Tran <jimmytran411@gmail.com>
2020-12-29 14:57:02 +0200

    modified Pet.js + add reach-router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import ReactDOM,{ render } from "react-dom";
+import { Link, Router } from "@reach/router";
 import Pet from "./Pet";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
     return (
         <div>
-            <h1 id="id">Adopt me!</h1>
-            <Pet name="Luna" animal="Dog" breed="Havanese" />
-            <SearchParams />
+            <header>
+                <Link to="/">Adopt Me!</Link>
+            </header>
+            <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+            </Router>
         </div>
     );
 }
